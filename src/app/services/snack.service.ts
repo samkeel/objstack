6617aba0b0df5a673bf1ace6acf51d565e481ac5
Sam <programming@vectors.net.au>
2021-02-22 20:54:47 +0800

    snackbar modified

--- src/app/services/snack.service.ts
+++ src/app/services/snack.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { tap } from 'rxjs/operators';
+import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackService {
 
-  constructor(private snackBar: MatSnackBar, private router: Router) { }
+  constructor(private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   authError() {
-    this.snackBar.open('You must be logged in!', 'OK', {
+    this.openSnackBar('You must be logged in!', 'Ok')
+      .onAction()
+      .subscribe(() => {
+        this.router.navigate(['/login'])
+      });
+
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 7000); //if user doesnt change route via navbar link. reroute page after 7seconds
+  }
+
+  openSnackBar(message: string, action: string): MatSnackBarRef<SimpleSnackBar> {
+    return this.snackBar.open(message, action, {
       duration: 5000
     });
-    return this.snackBar._openedSnackBarRef?.onAction()
-      .pipe(
-        tap(_ =>
-          this.router.navigate(['/login'])
-        )
-      )
-      .subscribe();
-
   }
+
 }
